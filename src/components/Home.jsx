b9b57a032e916b7cee219793f6a1dee5ce556793
yeeun234 <wooyeeun2@gmail.com>
2025-04-03 11:07:37 +0900

    -리액트 라우터 돔 설치 npminstall react-router-dom

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import Login from "./Login"
+import { Route, Routes ,BrowserRouter} from "react-router-dom"
+
 export default function Home() {
   return (
     <div className="w-full h-full flex justify-center items-center">
