61fb6d3ffcc2f926f280131bbf2b7dd0f59b4b37
yeeun234 <wooyeeun2@gmail.com>
2025-04-08 19:56:40 +0900

    fix: prevent error by avoiding direct rendering of object in JSX
    Realized that React can't render objects directly inside JSX. Updated code to render specific properties of the object instead of passing the entire object into a <div>.

--- src/components/Subway.jsx
+++ src/components/Subway.jsx
@@ -10,6 +10,7 @@ export default function Subway() {
     const ops = sarea.map((item) => {
         return <option value={item.코드} key={item.코드}>{item.측정소}</option>
     })
+    
     const [subwayData, setSubwayData] = useState();
     const [tags,setTags] = useState();
     const fetchSubwayData = async (code) => {
@@ -18,36 +19,55 @@ export default function Subway() {
         url = `${url}&pageNo=1&numOfRows=10&resultType=json`;
         url = `${url}&controlnumber=${new Date().toISOString().slice(0, 10).replaceAll('-','')}07`;
         url = `${url}&areaIndex=${code}`;
+        console.log(url);
         const response = await fetch(url);
         let data = await response.json();
-        data = data.response.body.items.item[0]
-        // setSubwayData(data);
-        console.log(data);  
-    };
-    useEffect(() => {
         
-        fetchSubwayData("203011");
-    }
-    , []);
-
-    useEffect(()=>{
-        if(subwayData)
-            return;
-
-    },[subwayData]);
+        data = data.response.body.items.item[0]; 
+       
+       setSubwayData(data);
+       
+    };
     const handleChange =()=>{
         console.log(selectRef.current.value);
-        fetchSubwayData(selectRef.current.value);
+        fetchSubwayData(selectRef.current.value);  
     };
     
-  return (
-    <div>
-      <select id='sel1' ref={selectRef} onChange={handleChange} className="mb-2 mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                 {ops} 
-            </select>
 
-        <div className='w-full h-20 flex justify-center items-center'>  
-            {subwayData}
+    // useEffect(()=>{
+    //     if(subwayData)
+    //         return;
+    //     let tm = Object.keys(scode);
+        
+    //     console.log(tm);
+    //     tm = tm.map((item)=>{
+    //         <div key={item}>
+    //             <div >
+    //                 {scode.item["name"]}
+    //             </div>
+    //             <div>
+    //                 ({item})
+    //             </div>
+    //         </div>
+    //     });
+    //     // setSubwayData(tm);
+    // },[subwayData]);
+
+    // useEffect(()=>{
+    //     const tm = data.map((item)=>{
+    //         <div></div>
+    //     });
+    // },[selectRef.current.value]);
+    
+  return (
+    <div className='flex flex-col justify-start items-center'>
+        <div>
+        <select id='sel1' ref={selectRef} onChange={handleChange} className="mb-2 mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                 {ops} 
+        </select>
+        </div>
+        <div className="w-full grid grid-cols-9 gap-4 mt-10">  
+            dd
         </div>
     </div>
   )
